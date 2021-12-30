fa84c2e812feebbff979a835a176c0d8f7a721be
hyjoong <maplesosser@naver.com>
2021-12-30 21:44:35 +0900

    test: snapshot testing & component test

---
+++ src/List.test.jsx
@@ -0,0 +1,35 @@
+import { render } from "@testing-library/react";
+import List from "./List";
+
+test("목록에 할일이 없을 수 있다.", () => {
+  const tasks = [];
+
+  const { container } = render(<List tasks={tasks} />);
+
+  expect(container).toHaveTextContent("할 일이 없습니다.");
+});
+
+test("할일 목록을 확인할 수 있다.", () => {
+  const tasks = [
+    {
+      id: 1,
+      title: "일 하는척 하면서 쇼핑하기",
+    },
+    {
+      id: 2,
+      title: "주식 보기",
+    },
+    {
+      id: 3,
+      title: "도지코인 풀 매수",
+    },
+  ];
+
+  const { container } = render(<List tasks={tasks} />);
+
+  expect(container).not.toHaveTextContent("할 일이 없습니다.");
+  tasks.forEach((task) => {
+    expect(container).toHaveTextContent(task.title);
+  });
+  expect(container.firstChild).toMatchSnapshot();
+});
